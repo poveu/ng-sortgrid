47379a6d9870eeae3007b1faeb2c60d1b1f18604
Pawe oboziak <pawel.loboziak@famoc.com>
2022-10-06 12:21:12 +0200

    Better sort grid item selector

--- projects/ng-sortgrid/src/lib/helpers/element/ngsg-elements.helper.ts
+++ projects/ng-sortgrid/src/lib/helpers/element/ngsg-elements.helper.ts
@@ -2,7 +2,7 @@ export class NgsgElementsHelper {
 
   public static findIndex(element: Element, parentElement?: Element): number {
     const allElements = parentElement
-      ? parentElement.querySelectorAll(element.tagName)
+      ? parentElement.querySelectorAll('[ngSortgridItem]')
       : element.parentNode.children;
 
     return Array.prototype.indexOf.call(allElements, element);
